af735e076e03c9a79e1960fa26f623e15c5f8228
Ale Sánchez <alesanchezmedina@gmail.com>
2020-04-20 19:36:28 +0200

    Modified header

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,20 +1,22 @@
 <template>
-  <div
-    class="flex flex-row uppercase flex-wrap sm:flex-no-wrap text-lg font-medium h-16 bg-gray-400"
-  >
-    <div class="p-4">
+  <div class="flex static md:absolute top-0 w-full">
+    <div class="px-4 py-3">
       <img class="h-8" src="~/assets/img/header/logo_512.png" />
     </div>
-    <nuxt-link to="/">
-      <div class="p-4">Inicio</div>
-    </nuxt-link>
-    <div class="p-4">Mini-Consejos</div>
-    <nuxt-link to="posts">
-      <div class="p-4">Cursos</div>
-    </nuxt-link>
-    <nuxt-link to="about">
-      <div class="p-4">Sobre Nosotros</div>
-    </nuxt-link>
+    <div
+      class="flex uppercase flex-wrap flex-grow justify-end sm:flex-no-wrap text-lg text-gray-300 h-16 bg-transparent"
+    >
+      <nuxt-link to="/">
+        <div class="p-4">Inicio</div>
+      </nuxt-link>
+      <div class="p-4">Mini-Consejos</div>
+      <nuxt-link to="posts">
+        <div class="p-4">Cursos</div>
+      </nuxt-link>
+      <nuxt-link to="about">
+        <div class="p-4">Sobre Nosotros</div>
+      </nuxt-link>
+    </div>
   </div>
 </template>
 
